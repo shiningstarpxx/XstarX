4febfd1018d505a6f17d7245e4a39dbd43bf6150
XStarX <shiningstarpxx@gmail.com>
2014-10-06 12:52:04 +0800

    stack & queue part

---
+++ ma/StackQueue/queue.c
@@ -0,0 +1,28 @@
+/*
+ * ====================================================================================
+ *
+ *       Filename:  queue.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  10/05/2014 15:45:42
+ *       Revision:  $Id$ 
+ *       Compiler:  gcc
+ *
+ *         Author:  michaelpei (XingxinPei), xingxinpei@gmai.com, xingxinpei@icloud.com
+ *        Company:  WDSGame
+ *
+ * =====================================================================================
+ */
+#include "queue.h"
+
+int queue_enqueue( Queue *queue, const void *data )
+{
+	return list_ins_next(queue, list_tail(queue), data);
+}
+
+int queue_dequeue( Queue *queue, void **data )
+{
+	return list_rem_next(queue, NULL, data );
+}
---
+++ ma/StackQueue/queue.h
@@ -0,0 +1,35 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  queue.h
+ *
+ *    Description:  queue implemented by list
+ *
+ *        Version:  1.0
+ *        Created:  10/05/2014 15:41:48
+ *       Revision:  $Id$ 
+ *       Compiler:  gcc
+ *
+ *         Author:  michaelpei (XingxinPei), xingxinpei@gmai.com, xingxinpei@icloud.com
+ *        Company:  WDSGame
+ *
+ * =====================================================================================
+ */
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include <stdlib.h>
+#include "../list/list.h"
+
+typedef List Queue;
+
+#define queue_init list_init
+#define queue_destroy list_destroy
+
+int queue_enqueue( Queue *queue, const void *data );
+int queue_dequeue( Queue *queue, void **data );
+
+#define queue_peek(queue) ((queue)->head == NULL ? NULL : (queue)->head->data )
+#define queue_size list_size
+
+#endif
